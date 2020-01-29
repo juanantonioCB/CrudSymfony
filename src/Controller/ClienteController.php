caaee3ffb58cb287afbc7f38d3f5a54df49881d9
Juan Antonio Cabello <juanantonio186@hotmail.com>
2020-01-29 16:09:01 +0100

    primero

---
+++ src/Controller/ClienteController.php
@@ -0,0 +1,11 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class ClienteController extends AbstractController{
+    /**
+     * @Route("/clientes", name="cliente_index", methods={"GET"})
+     */
+}
